d84c3518cc2fe03b44b27c01d79ecf107337c1fe
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-06-19 04:41:05 -0400

    Add wallet with key and refactor transaction instance and minePendingTransactions method

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-import { Blockchain, Transaction } from './src/blockchain'; 
+import { Blockchain, Transaction } from './src/blockchain';
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
+const myKey = ec.keyFromPrivate('97897979874f07c8872b56d869c99cc0f6444e9d05695c2fef8ce8d3f9ebc988');
+const myWalletAddress = myKey.getPublic('hex');
 
 // Instance of Blockchain
 const docCoin = new Blockchain();
 
 // Transactions
-docCoin.createTransaction( new Transaction('address1', 'address2', 100));
-docCoin.createTransaction( new Transaction('address2', 'address1', 50));
+const trans1 = new Transaction(myWalletAddress, 'place key here', 20);
+trans1.signTransaction(myKey);
+docCoin.addTransaction(trans1);
+
 
 console.log('\n Starting the miner...');
-docCoin.minePendingTransactions('napoleons-address');
+docCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance in Napoleon is ', docCoin.getBalanceOfAddress('napoleon-address'));
+console.log('\nBalance in Napoleon Wallet is ', docCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\n Starting the miner to move transaction out of pending into wallet...');
-docCoin.minePendingTransactions('napoleons-address');
+// console.log('\n Starting the miner to move transaction out of pending into wallet...');
+// docCoin.minePendingTransactions('napoleons-address');
 
-console.log('\nBalance in Napoleon is ', docCoin.getBalanceOfAddress('napoleons-address'));
+// console.log('\nBalance in Napoleon is ', docCoin.getBalanceOfAddress('napoleons-address'));
 
 // console.log('Mining block 1...');
 // docCoin.addBlock(new Block(1, "06/12/2019", { amount: 10 }));
